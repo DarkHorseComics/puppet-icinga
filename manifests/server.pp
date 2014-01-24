af2f09acb1741aa00f42550a03126754b72cef57
Nick Chappell <nick@intronic.org>
2014-01-23 23:45:57 -0800

    Added more notes to server.pp.

--- manifests/server.pp
+++ manifests/server.pp
@@ -8,3 +8,25 @@
 
 # copied a file with the command below, but I could template it
 # cp /usr/share/doc/icinga-idoutils/examples/idoutils.cfg-sample /etc/icinga/modules/idoutils.cfg
+
+#set IDO2DB to yes in /etc/icinga/default
+#Perms are -rw-r--r-- 1 root root 340 Jan 23 22:38 /etc/default/icinga
+
+
+
+#/etc/icinga/icinga.cfg
+#Perms: -rw-r--r-- 1 root root 51K Jan 23 22:40 /etc/icinga/icinga.cfg
+#Things I set:
+
+# check_external_commands=1
+
+
+#Exec resource commands to run:
+
+# dpkg-statoverride --update --add nagios www-data 2710 /var/lib/icinga/rw; touch /etc/icinga/exec-override-icinga-rw-www_is_complete
+
+#dpkg-statoverride --update --add nagios nagios 751 /var/lib/icinga; touch touch /etc/icinga/override-icinga-lib-www_is_complete
+
+#load schema
+
+# psql -U postgres -d icinga < /usr/share/dbconfig-common/data/icinga-idoutils/install/pgsql
