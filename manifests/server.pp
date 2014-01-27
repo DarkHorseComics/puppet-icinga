79c40056038995affd561a208415bbf516232685
Nick Chappell <nick@intronic.org>
2014-01-26 22:02:29 -0800

    Added more file and command notes to the server.pp manifest.

--- manifests/server.pp
+++ manifests/server.pp
@@ -8,6 +8,8 @@
 
 # copied a file with the command below, but I could template it
 # cp /usr/share/doc/icinga-idoutils/examples/idoutils.cfg-sample /etc/icinga/modules/idoutils.cfg
+# -rw-r--r-- 1 root root 1.3K Jan 23 22:22 /etc/icinga/modules/idoutils.cfg
+
 
 #set IDO2DB to yes in /etc/icinga/default
 #Perms are -rw-r--r-- 1 root root 340 Jan 23 22:38 /etc/default/icinga
@@ -20,17 +22,16 @@
 
 #/etc/icinga/icinga.cfg
 #Perms: -rw-r--r-- 1 root root 51K Jan 23 22:40 /etc/icinga/icinga.cfg
-#Things I set:
-
-# check_external_commands=1
+  #Things I set:
+    # check_external_commands=1
 
 
 #Exec resource commands to run:
 
-# dpkg-statoverride --update --add nagios www-data 2710 /var/lib/icinga/rw; touch /etc/icinga/exec-override-icinga-rw-www_is_complete
+#dpkg-statoverride --update --add nagios www-data 2710 /var/lib/icinga/rw; touch /etc/icinga/exec-override-icinga-rw-www_is_complete
 
 #dpkg-statoverride --update --add nagios nagios 751 /var/lib/icinga; touch touch /etc/icinga/override-icinga-lib-www_is_complete
 
-#load schema
+#load DB schema
 
-# psql -U postgres -d icinga < /usr/share/dbconfig-common/data/icinga-idoutils/install/pgsql
+# su postgres -c 'psql -d icinga < /usr/share/dbconfig-common/data/icinga-idoutils/install/pgsql'
