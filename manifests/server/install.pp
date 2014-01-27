a362b74e56036fa55543ec9d201bd1f2cd20c615
Nick Chappell <nick@intronic.org>
2014-01-26 22:28:17 -0800

    Added a case statement for adding the right package repo based on the operating system.

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -7,10 +7,20 @@
 
 #Packages
 
-  #case statement goes here for adding the right package repo based on the OS.
+  
 
   #case statement goes here for selecting the package names based on the OS.
 
 class icinga::server::install {
+
+  #case statement goes here for adding the right package repo based on the OS.
+  case $operatingsystem {
+    #Add the yum repo for Red Had and CentOS systems:
+    'RedHat', 'CentOS': {} 
+    #Add the Icinga PPA for Debian/Ubuntu systems:
+    /^(Debian|Ubuntu)$/: {}
+    #Fail if we're on any other OS:
+    default: { fail("${operatingsystem} is not supported!") } 
+  }
 
 }
