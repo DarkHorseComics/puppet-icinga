e036e4f0424b871184044417f978a80bf4d884f2
Nick Chappell <nick@intronic.org>
2014-01-26 22:57:02 -0800

    Added a section for database settings; added a few default parameters.

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,5 +5,19 @@
 # Parameters: See the inline comments.
 
 class icinga::params {
+ 
+  ##################
+  #Database settings
+  ##################
+  
+  #server_db_password is left blank deliberately.
+  #Since putting database passwords in your manifests/modules is a bad idea,
+  #you should get the DB password via a Hiera lookup.
+  $server_db_type        = 'postgres'
+  $server_db_host        = 'localhost'
+  $server_db_port        = '5432'
+  $server_db_user        = 'icinga'
+  $server_db_password    = undef
+  
 
 }
