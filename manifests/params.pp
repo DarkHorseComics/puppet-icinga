f8e339919ef203d4e51fe0f11604e2174b6508ff
Nick Chappell <nick@intronic.org>
2014-01-26 22:48:22 -0800

    Added a params manifest and class.

---
+++ manifests/params.pp
@@ -0,0 +1,9 @@
+# Class: icinga::server::service
+#
+# This class manges the parameters used to configure the server components of Icinga.
+#
+# Parameters: See the inline comments.
+
+class icinga::params {
+
+}
