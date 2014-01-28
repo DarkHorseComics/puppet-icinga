d77a9f14e518008201321d0953849f4b68b38992
Nick Chappell <nick@intronic.org>
2014-01-27 21:03:08 -0800

    Add an httpauth resource for the icingaadmin user.

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -7,5 +7,15 @@
 class icinga::server::config {
 
   include icinga::params
+  
+  #Create an htpasswd entry for the 'icingaadmin' user. This requires the httpauth module
+  #to be installed: https://github.com/jamtur01/puppet-httpauth
+  httpauth { 'icingaadmin':
+    file      => '/etc/icinga/passwd',
+    password  => $icinga::params::icingaadmin_password,
+    realm     => 'realm',
+    mechanism => basic,
+    ensure    => present,
+  }
 
 }
