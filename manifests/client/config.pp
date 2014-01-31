315ca35d095bd6d8bfd6a94885b01e5b50215172
Nick Chappell <nick@intronic.org>
2014-01-30 20:44:52 -0800

    Added default parameters from the nrpe.cfg files for each OS type; added a file resource for the /etc/nagios directory.

--- manifests/client/config.pp
+++ manifests/client/config.pp
@@ -3,9 +3,17 @@
 # This subclass configures Icinga clients.
 #
 
-class icinga::client::config () 
+class icinga::client::config 
 inherits icinga::params {
   
   #config resources here
+ 
+  #The NRPE configuration base directory:
+  file { $nrpe_config_basedir:
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '755',
+  }
 
 }
