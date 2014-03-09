c4c839b25b76fd9783bbc8714b9fe90a9430de27
Nick Chappell <nick@intronic.org>
2014-03-09 14:52:35 -0700

    Added a directory resource to make a folder that will hold host escalation definitions.

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -127,6 +127,15 @@ class icinga::server::config {
     recurse => true,
   }
 
+  #A folder for host escalation definitions:
+  file { '/etc/icinga/objects/hostescalations':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '755',
+    recurse => true,
+  }
+
   #A folder for hostgroup definitions:
   file { '/etc/icinga/objects/hostgroups':
     ensure  => directory,
