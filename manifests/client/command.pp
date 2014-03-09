2030956dad13248a5fcf6d7cfa07239a263b3138
Nick Chappell <nick@intronic.org>
2014-03-08 19:53:18 -0800

    Added in-manifest documentation to the NRPE command defined type.

--- manifests/client/command.pp
+++ manifests/client/command.pp
@@ -1,4 +1,13 @@
 # Define icinga::client::command
+#
+# This defined type creates NRPE command definitions on machines running NRPE.
+#
+# Parameters:
+# * $command_name = What NRPE will know the command as; this defaults to the title of the resource
+# * $nrpe_plugin_liddir = The directory where the NRPE plugins themselves live
+# * $nrpe_plugin_name = The name of the plugin the command will run
+# * $nrpe_plugin_args = The arguments to pass to the plugin. This may be optional, 
+#                       depending on the plugin and whether it expects any arguments or parameters
 
 define icinga::client::command (
   $command_name       = $name,
