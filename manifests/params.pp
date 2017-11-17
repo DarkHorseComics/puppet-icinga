40fa62dd36a034903517ff123e62f9e12ab5e5a7
Andy Wood <andyw@darkhorse.com>
2017-11-17 15:39:56 -0800

    Include nagions-plugins-{common,contrib} in xenial too

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,12 +5,12 @@
 # Parameters: See the inline comments.
 
 class icinga::params {
- 
- 
+
+
   ##############################
   # Configuration parameters
   ##############################
- 
+
   ##################
   # Database settings
   #The server_db_password is left blank deliberately.
@@ -23,13 +23,13 @@ class icinga::params {
   $server_db_password   = undef
   $server_db_name       = 'icinga'
   $server_users         = ['icingaadmin']
-  
+
   ##################
   # Icinga server settings
   #The default icingaadmin password.
   #Default value from: https://xkcd.com/936/
   $icingaadmin_password = 'horsebatterystaple'
-  
+
   case $operatingsystem {
     #File and template variable names for Red Had/CentOS systems:
     'RedHat', 'CentOS': {}
@@ -45,7 +45,7 @@ class icinga::params {
   }
 
   ##################
-  # Icinga client settings 
+  # Icinga client settings
   $nrpe_listen_port        = '5666'
   $nrpe_log_facility       = 'daemon'
   $nrpe_debug_level        = '0'
@@ -53,9 +53,9 @@ class icinga::params {
   $nrpe_command_timeout    = '60'
   #in seconds:
   $nrpe_connection_timeout = '300'
-  #Note: because we use .join in the nrpe.cfg.erb template, this value *must* be an array 
+  #Note: because we use .join in the nrpe.cfg.erb template, this value *must* be an array
   $nrpe_allowed_hosts      = ['127.0.0.1',]
-   
+
   case $operatingsystem {
     #File and template variable names for Red Had/CentOS systems:
     'RedHat', 'CentOS': {
@@ -79,9 +79,9 @@ class icinga::params {
 
   ##################
   # Icinga web parameters
-  #How often to automatically refresh the web UI, in seconds: 
+  #How often to automatically refresh the web UI, in seconds:
   $web_ui_refresh_rate = "90"
-  #How many results in large lists of hosts, services or check results to display per page: 
+  #How many results in large lists of hosts, services or check results to display per page:
   $web_ui_results_per_page_limit = "200"
   #The character to use as a delimiter in CSV files that are exported:
   $exported_csv_delimiter_char   = ";"
@@ -97,33 +97,33 @@ class icinga::params {
         'pgsql': { $lib_db_package = 'icinga-idoutils-libdbi-pgsql'}
         default: { fail("${icinga::params::server_db_type} is not supported!") }
       }
-      
+
       #Pick the right pacakage provider:
       $package_provider = 'yum'
       #Finally, pick the right list of packages:
       $icinga_client_packages = ["nrpe", "nagios-plugins-nrpe", "nagios-plugins-all", "nagios-plugins-openmanage", "nagios-plugins-check-updates"]
-    } 
-    #Debian/Ubuntu systems: 
+    }
+    #Debian/Ubuntu systems:
     /^(Debian|Ubuntu)$/: {
       #Pick the right DB lib package name based on the database type the user selected:
       case $icinga::params::server_db_type {
         'mysql': { $lib_db_package = 'libdbd-mysql'}
         'pgsql': { $lib_db_package = 'libdbd-pgsql'}
-        default: { fail("${icinga::params::server_db_type} is not supported!") } 
+        default: { fail("${icinga::params::server_db_type} is not supported!") }
       }
       #Pick the right pacakage provider:
       $package_provider = 'apt'
       #Finally, pick the right list of packages:
-      
+
       #Ubuntu 12.04 doesn't have nagios-plugins-common or nagios-plugins-contrib packages available...
       case $operatingsystemrelease {
         '12.04': {
           $icinga_server_packages = ["icinga", "icinga-doc", "icinga-idoutils", "nagios-nrpe-server", "nagios-plugins", "nagios-plugins-basic", "nagios-plugins-standard", "nagios-snmp-plugins", "nagios-plugins-extra", "nagios-nrpe-plugin", $lib_db_package]
           $icinga_client_packages = ["nagios-nrpe-server", "nagios-plugins", "nagios-plugins-basic", "nagios-plugins-standard", "nagios-snmp-plugins", "nagios-plugins-extra"]
-        
+
         }
         #...but 13.10 does:
-        '13.10', '14.04': {
+        '13.10', '14.04', '16.04': {
           $icinga_server_packages = ["icinga", "icinga-doc", "icinga-idoutils", "nagios-nrpe-server", "nagios-plugins", "nagios-plugins-basic", "nagios-plugins-common", "nagios-plugins-standard", "nagios-snmp-plugins", "nagios-plugins-extra", "nagios-plugins-contrib", "nagios-nrpe-plugin", $lib_db_package]
           $icinga_client_packages = ["nagios-nrpe-server", "nagios-plugins", "nagios-plugins-basic", "nagios-plugins-common", "nagios-plugins-standard", "nagios-snmp-plugins", "nagios-plugins-extra", "nagios-plugins-contrib"]
         }
